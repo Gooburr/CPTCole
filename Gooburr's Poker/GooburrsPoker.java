8a76183911f93106a4356112082726ca5a0a9f40
Gooburr <130682584+Gooburr@users.noreply.github.com>
2023-06-07 22:19:04 -0400

    added sorting of hand, ability to set test values for the cards. every check for each type of hand, and debug notes that tell me which checks succeed

--- Gooburr's Poker/GooburrsPoker.java
+++ Gooburr's Poker/GooburrsPoker.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 public class GooburrsPoker{
 	static Console con = new Console("Gooburr's Poker",1280,720);
 	static int intDeck[][] = new int[52][3];
+	static int intHand[][] = new int[5][2];
 	static BufferedImage imgBG;
 	static BufferedImage imgCards[] = new BufferedImage[5];
 	static boolean blnCardsSwap[] = new boolean[5];
@@ -22,8 +23,23 @@ public class GooburrsPoker{
 		sortDeck();
 		boolean blnRedraw = true;
 		imgBG = con.loadImage("images/PokerBG.jpg");
+		//THE NEXT 10 LINES ARE FOR TESTING
+		intHand[0][0] = 2;
+		intHand[0][1] = 0;
+		intHand[1][0] = 2;
+		intHand[1][1] = 3;
+		intHand[2][0] = 2;
+		intHand[2][1] = 1;
+		intHand[3][0] = 2;
+		intHand[3][1] = 2;
+		intHand[4][0] = 4;
+		intHand[4][1] = 1;
 		for(int intCount = 0; intCount < 5; intCount++){
-			imgCards[intCount] = con.loadImage("images/cards/"+intDeck[intCount][0]+"_"+intDeck[intCount][1]+".png");
+			/*
+			intHand[intCount][0] = intDeck[intCount][0];
+			intHand[intCount][1] = intDeck[intCount][1];
+			*/
+			imgCards[intCount] = con.loadImage("images/cards/"+intHand[intCount][0]+"_"+intHand[intCount][1]+".png");
 			blnCardsSwap[intCount] = false;
 		}
 		while(!clickButton(420,570,480,110)){
@@ -49,13 +65,34 @@ public class GooburrsPoker{
 				blnRedraw = false;
 			}
 		}
+		intTotalCount = 5;
+		con.drawImage(imgBG,0,0);
+		for(int intCount = 0; intCount < 5; intCount++){
+			if(blnCardsSwap[intCount]){
+				intHand[intCount][0] = intDeck[intTotalCount][0];
+				intHand[intCount][1] = intDeck[intTotalCount][1];
+				imgCards[intCount] = con.loadImage("images/cards/"+intHand[intCount][0]+"_"+intHand[intCount][1]+".png");
+				intTotalCount++;
+			}
+			con.drawImage(imgCards[intCount],(intCount+1)*120+250,420);
+		}
+		con.repaint();
+		con.sleep(2000);
+		sortHand();
+		con.drawImage(imgBG,0,0);
+		for(int intCount = 0; intCount < 5; intCount++){
+			imgCards[intCount] = con.loadImage("images/cards/"+intHand[intCount][0]+"_"+intHand[intCount][1]+".png");
+			con.drawImage(imgCards[intCount],(intCount+1)*120+250,420);
+		}
+		con.repaint();
+		System.out.println("pair: "+pair()+" two pairs: "+twoPairs()+" 3OAK: "+threeOAK()+" straight: "+straight()+" flush: "+flush()+" full house: "+fullHouse()+" 4OAK: "+fourOAK()+" straight flush: "+straightFlush());
 	}
 	public static void sortDeck(){
 		int intTemp;
 		int intBelow;
 		int intCurrent;
-		for(int intCount = 0; intCount < 51; intCount++){
-			for(int intCount2 = 0; intCount2 < 51-intCount-1; intCount2++){
+		for(int intCount = 0; intCount < 52; intCount++){
+			for(int intCount2 = 0; intCount2 < 52-intCount-1; intCount2++){
 				intBelow = intDeck[intCount2+1][2];
 				intCurrent = intDeck[intCount2][2];
 				if(intBelow > intCurrent){
@@ -72,7 +109,76 @@ public class GooburrsPoker{
 			}
 		}
 	}
+	public static void sortHand(){
+		int intTemp;
+		int intBelow;
+		int intCurrent;
+		for(int intCount = 0; intCount < 5; intCount++){
+			for(int intCount2 = 0; intCount2 < 5-intCount-1; intCount2++){
+				intBelow = intHand[intCount2+1][0];
+				intCurrent = intHand[intCount2][0];
+				if(intBelow > intCurrent){
+					intTemp = intBelow;
+					intHand[intCount2+1][0] = intHand[intCount2][0];
+					intHand[intCount2][0] = intTemp;
+					intTemp = intHand[intCount2+1][1];
+					intHand[intCount2+1][1] = intHand[intCount2][1];
+					intHand[intCount2][1] = intTemp;
+					
+				}
+			}
+		}
+		
+	}
 	public static boolean clickButton(int intPosX, int intPosY, int intWidth, int intHeight){
 		return con.currentMouseButton() == 1 && con.currentMouseX() >= intPosX && con.currentMouseX() <= intPosX + intWidth && con.currentMouseY() >= intPosY && con.currentMouseY() <= intPosY + intHeight;
+	}
+	//Different hand types
+	public static boolean pair(){
+		for(int intCount = 0; intCount < 4; intCount++){
+			if(intHand[intCount][0] == intHand[intCount+1][0] && intHand[intCount][0] > 10){
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean twoPairs(){
+		int intPairs = 0;
+		for(int intCount = 0; intCount < 4; intCount++){
+			if(intHand[intCount][0] == intHand[intCount+1][0]){
+				intPairs++;
+				//intCount++ is to make sure it doesn't count 3 of a kinds
+				intCount++;
+			}
+		}
+		return intPairs == 2;
+	}
+	public static boolean threeOAK(){
+		for(int intCount = 0; intCount < 3; intCount++){
+			if(intHand[intCount][0] == intHand[intCount+1][0] && intHand[intCount][0] == intHand[intCount+2][0]){
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean straight(){
+		return intHand[0][0] == intHand[1][0]+1 && intHand[0][0] == intHand[2][0]+2 && intHand[0][0] == intHand[3][0]+3 && intHand[0][0] == intHand[4][0]+4;
+	}
+	public static boolean flush(){
+		return intHand[0][1] == intHand[1][1] && intHand[0][1] == intHand[2][1] && intHand[0][1] == intHand[3][1] && intHand[0][1] == intHand[4][1];
+	}
+	public static boolean fullHouse(){
+		return threeOAK() && twoPairs();
+	}
+	public static boolean fourOAK(){
+		for(int intCount = 0; intCount < 2; intCount++){
+			if(intHand[intCount][0] == intHand[intCount+1][0] && intHand[intCount][0] == intHand[intCount+2][0] && intHand[intCount][0] == intHand[intCount+3][0]){
+				return true;
+			}
+		}
+		return false;
+	}
+	public static boolean straightFlush(){
+		return straight() && flush();
 	}
 }
