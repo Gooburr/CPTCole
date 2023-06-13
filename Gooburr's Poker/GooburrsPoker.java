67933b500a79bf6064731f354f4f0c32831ce382
Gooburr <130682584+Gooburr@users.noreply.github.com>
2023-06-13 13:17:30 -0400

    added high score and help page, added secret joke, and added comments

--- Gooburr's Poker/GooburrsPoker.java
+++ Gooburr's Poker/GooburrsPoker.java
@@ -3,24 +3,35 @@ import java.awt.image.BufferedImage;
 import java.awt.Color;
 
 public class GooburrsPoker{
+	//set up static variables
 	static Console con = new Console("Gooburr's Poker",1280,720);
 	static int intDeck[][] = new int[52][3];
 	static int intHand[][] = new int[5][2];
 	static BufferedImage imgBG;
 	static BufferedImage imgCards[] = new BufferedImage[5];
 	static boolean blnCardsSwap[] = new boolean[5];
+	static char chrInput = ' ';
 	
 	public static void main(String[] args){
+		//set up screen
 		imgBG = con.loadImage("images/menu.jpg");
 		con.drawImage(imgBG,0,0);
 		con.repaint();
-		while(con.getChar() != 'p'){
-			if(con.getChar() == 'h'){
+		boolean blnMenu = true;
+		//menu key input
+		while(chrInput != 'p'){
+			chrInput = con.getChar();
+			if(chrInput == 'h'){
 				help();
-			}else if(con.getChar() == 'v'){
+			}
+			if(chrInput == 'v'){
 				viewScores();
 			}
+			if(chrInput == 's'){
+				secret();
+			}
 		}
+		//name input
 		con.print("Enter your name: ");
 		String strName = con.readLine();
 		int intMoney;
@@ -30,6 +41,7 @@ public class GooburrsPoker{
 			intMoney = 1000;
 		}
 		con.clear();
+		//show game background and the backs of the cards
 		imgBG = con.loadImage("images/PokerBG.jpg");
 		con.drawImage(imgBG,0,0);
 		for(int intCount = 0; intCount < 5; intCount++){
@@ -37,6 +49,7 @@ public class GooburrsPoker{
 			con.drawImage(imgCards[intCount],(intCount+1)*120+250,420);
 		}
 		con.repaint();
+		//the player inputs their bet
 		int intBet = 99999999;
 		while(intBet > intMoney || intBet <= 0){
 			con.println("Name: "+strName);
@@ -58,6 +71,7 @@ public class GooburrsPoker{
 		con.println("Name: "+strName);
 		con.println("Money: $"+intMoney);
 		con.println("Bet: $"+intBet);
+		//the deck is set up, and shuffled
 		int intTotalCount = 0;
 		for(int intCount = 0; intCount <= 3; intCount++){
 			for(int intCount2 = 1; intCount2 <= 13; intCount2++){
@@ -68,12 +82,19 @@ public class GooburrsPoker{
 			}
 		}
 		sortDeck();
+		//variables are set up for the main gameplay loop
 		boolean blnPlaying = true;
 		boolean blnRedraw;
 		String strInput;
+		//intHovering shows what the player is hovering on, 0 is nothing, 1-5 are the respective non-swap pile cards, 6-10 are the respective swap pile cards, 11 is ready button
+		int intHover = 0;
+	
+		//set the draw color for the hover button outline
+		con.setDrawColor(Color.YELLOW);
+		//main gameplay loop
 		while(blnPlaying){
 			blnRedraw = true;
-			//THE NEXT 10 LINES ARE FOR TESTING
+			//THE NEXT 12 LINES ARE FOR TESTING
 			/*
 			intHand[0][0] = 2;
 			intHand[0][1] = 0;
@@ -86,13 +107,20 @@ public class GooburrsPoker{
 			intHand[4][0] = 4;
 			intHand[4][1] = 1;
 			*/ 
+			//hand is set up
 			for(int intCount = 0; intCount < 5; intCount++){
 				intHand[intCount][0] = intDeck[intCount][0];
 				intHand[intCount][1] = intDeck[intCount][1];
 				imgCards[intCount] = con.loadImage("images/cards/"+intHand[intCount][0]+"_"+intHand[intCount][1]+".png");
 				blnCardsSwap[intCount] = false;
 			}
+			//loop is repeated while the player is not clicking the ready button
 			while(!clickButton(420,570,480,110)){
+				//check if the player is clicking a card to swap it, and hovering a card/ready button to draw the outline
+				if(hoverButton(420,570,480,110) && intHover == 0){
+					intHover = 11;
+					blnRedraw = true;
+				}
 				for(int intCount = 0; intCount < 5; intCount++){
 					if(clickButton((intCount+1)*120+250,420,71,96) && !blnCardsSwap[intCount]){
 						blnCardsSwap[intCount] = !blnCardsSwap[intCount];
@@ -101,7 +129,24 @@ public class GooburrsPoker{
 						blnCardsSwap[intCount] = !blnCardsSwap[intCount];
 						blnRedraw = true;
 					}
+					if(hoverButton((intCount+1)*120+250,420,71,96) && !blnCardsSwap[intCount] && intHover == 0){
+						intHover = intCount+1;
+						blnRedraw = true;
+					}else if(hoverButton((intCount+1)*120+250,240,71,96) && blnCardsSwap[intCount] && intHover == 0){
+						intHover = intCount+6;
+						blnRedraw = true;
+					}
 				}
+				//Imma just be real w/ u there's prolly an easier way to do this whole check but I'm too lazy to figure it out and this works ¯\_(ツ)_/¯
+				if(!hoverButton((1)*120+250,240,71,96) &&  !hoverButton((2)*120+250,240,71,96) && 
+				!hoverButton((3)*120+250,240,71,96) && !hoverButton((4)*120+250,240,71,96) && !hoverButton((5)*120+250,240,71,96) && 
+				!hoverButton((1)*120+250,420,71,96) && !hoverButton((2)*120+250,420,71,96) &&
+				!hoverButton((3)*120+250,420,71,96) && !hoverButton((4)*120+250,420,71,96) && !hoverButton((5)*120+250,420,71,96) &&
+				!hoverButton(420,570,480,110) && intHover != 0){
+					intHover = 0;
+					blnRedraw = true;
+				}
+				//redraw the card to put it in the swap pile
 				if(blnRedraw){
 					con.drawImage(imgBG,0,0);
 					for(int intCount = 0; intCount < 5; intCount++){
@@ -110,11 +155,20 @@ public class GooburrsPoker{
 						}else{
 							con.drawImage(imgCards[intCount],(intCount+1)*120+250,240);
 						}
+						if(hoverButton((intCount+1)*120+250,420,71,96) && !blnCardsSwap[intCount]){
+							con.drawRect((intCount+1)*120+249,419,73,98);
+						}else if(hoverButton((intCount+1)*120+250,240,73,98) && blnCardsSwap[intCount]){
+							con.drawRect((intCount+1)*120+249,239,73,98);
+						}
+					}
+					if(hoverButton(420,570,480,110)){
+						con.drawRect(420,569,475,112);
 					}
 					con.repaint();
 					blnRedraw = false;
 				}
 			}
+			//draw the new hand with the swapped cards
 			intTotalCount = 5;
 			con.drawImage(imgBG,0,0);
 			for(int intCount = 0; intCount < 5; intCount++){
@@ -127,6 +181,7 @@ public class GooburrsPoker{
 				con.drawImage(imgCards[intCount],(intCount+1)*120+250,420);
 			}
 			con.repaint();
+			//after 2 seconds, sort the hand and draw the sorted hand
 			con.sleep(2000);
 			sortHand();
 			con.drawImage(imgBG,0,0);
@@ -135,6 +190,7 @@ public class GooburrsPoker{
 				con.drawImage(imgCards[intCount],(intCount+1)*120+250,420);
 			}
 			con.repaint();
+			//check which state the deck is in, and give the appropriate amount of money to the player
 			System.out.println("pair: "+pair()+" two pairs: "+twoPairs()+" 3OAK: "+threeOAK()+" straight: "+straight()+" flush: "+flush()+" full house: "+fullHouse()+" 4OAK: "+fourOAK()+" straight flush: "+straightFlush());
 			con.clear();
 			if(straightFlush()){
@@ -173,6 +229,7 @@ public class GooburrsPoker{
 			}
 			con.sleep(2000);
 			con.clear();
+			//check if the player wishes to play again
 			if(intMoney != 0){
 				con.println("Name: "+strName);
 				con.println("Money: $"+intMoney);
@@ -183,13 +240,16 @@ public class GooburrsPoker{
 				con.println("You're broke! you can't play anymore!");
 				strInput = "not play";
 			}
+			//run if the player wishes to play again
 			if(strInput.equalsIgnoreCase("play")){
+				//shuffle the deck
 				for(int intCount = 0; intCount < 52; intCount++){
 					intDeck[intCount][2] = (int)(Math.random()*100.0 + 1.0);
 				}
 				sortDeck();
+				//ask for the player's bet
 				intBet = 99999999;
-				while(intBet > intMoney){
+				while(intBet > intMoney || intBet <= 0){
 					con.println("Name: "+strName);
 					con.println("Money: $"+intMoney);
 					con.print("Input your bet: $");
@@ -198,21 +258,74 @@ public class GooburrsPoker{
 					if(intBet > intMoney){
 						con.println("HEY! You can't bet more money than you acually have!");
 					}
+					if(intBet < 0){
+						con.println("HEY! You can't bet a negative amount of money!");
+					}
+					if(intBet == 0){
+						con.println("HEY! You can't bet nothing!");
+					}
 				}
 				intMoney -= intBet;
 				con.println("Name: "+strName);
 				con.println("Money: $"+intMoney);
 				con.println("Bet: $"+intBet);
 				con.drawImage(imgBG,0,0);
+				//set up and draw the player's hand
 				for(int intCount = 0; intCount < 5; intCount++){
 					imgCards[intCount] = con.loadImage("images/cards/back.png");
 					con.drawImage(imgCards[intCount],(intCount+1)*120+250,420);
 				}
+				//since we are in a while loop, and blnPlaying is true, the loop will restart
+			//if the player doesn't wish to play again
 			}else{
+				//set up variables related to saving the player's score
+				TextInputFile txtScoresInput = new TextInputFile("scores.txt");
+				TextOutputFile txtScoresOutput = new TextOutputFile("scores.txt");
+				String strNames[] = new String[25];
+				int intScores[] = new int[25];
+				intTotalCount = 0;
+				int intScoresCount;
+				//set the strNames and intScores array to the scores listed in scores.txt
+				while(!txtScoresInput.eof() && intTotalCount < 25){
+					strNames[intTotalCount] = txtScoresInput.readLine();
+					intScores[intTotalCount] = txtScoresInput.readInt();
+					intTotalCount++;
+				}
+				intScoresCount = intTotalCount;
+				System.out.println("scores count: "+intScoresCount);
+				//if there's no scores, just print out the player's current score
+				if(intScoresCount == 0){
+					txtScoresOutput.println(strName);
+					txtScoresOutput.println(intMoney);
+				//else, if the players score is bigger than the last score, insert it in where it belongs, scores are ranked from highest score to lowest
+				}else if(intMoney > intScores[intScoresCount]){
+					strNames[intScoresCount] = strName;
+					intScores[intScoresCount] = intMoney;
+					int intTemp;
+					String strTemp;
+					while(intMoney > intScores[intTotalCount-1] && intTotalCount > 1){
+						intTemp = intScores[intTotalCount];
+						intScores[intTotalCount] = intScores[intTotalCount-1];
+						intScores[intTotalCount-1] = intTemp;
+						strTemp = strNames[intTotalCount];
+						strNames[intTotalCount] = strNames[intTotalCount-1];
+						strNames[intTotalCount-1] = strTemp;
+						intTotalCount--;
+					}
+					for(int intCount = 0; intCount <= intScoresCount; intCount++){
+						txtScoresOutput.println(strNames[intCount]);
+						txtScoresOutput.println(intScores[intCount]);
+						System.out.println(strNames[intCount]);
+						System.out.println(intScores[intCount]);
+					}
+				}
+				//exit the while loop
 				blnPlaying = false;
+				con.println("your score has been saved!");
 			}
 		}
 	}
+	//sort the deck based on the random int, essentially shuffling it
 	public static void sortDeck(){
 		int intTemp;
 		int intBelow;
@@ -235,6 +348,7 @@ public class GooburrsPoker{
 			}
 		}
 	}
+	//sort the hand so it is easier to categorize
 	public static void sortHand(){
 		int intTemp;
 		int intBelow;
@@ -250,22 +364,103 @@ public class GooburrsPoker{
 					intTemp = intHand[intCount2+1][1];
 					intHand[intCount2+1][1] = intHand[intCount2][1];
 					intHand[intCount2][1] = intTemp;
-					
 				}
 			}
 		}
 		
 	}
+	//check if the user is clicking or hovering over a rectangular area on the screen
 	public static boolean clickButton(int intPosX, int intPosY, int intWidth, int intHeight){
 		return con.currentMouseButton() == 1 && con.currentMouseX() >= intPosX && con.currentMouseX() <= intPosX + intWidth && con.currentMouseY() >= intPosY && con.currentMouseY() <= intPosY + intHeight;
 	}
+	public static boolean hoverButton(int intPosX, int intPosY, int intWidth, int intHeight){
+		return con.currentMouseX() >= intPosX && con.currentMouseX() <= intPosX + intWidth && con.currentMouseY() >= intPosY && con.currentMouseY() <= intPosY + intHeight;
+	}
+	//methods that show the help and scores screens respectively
 	public static void help(){
-		
+		System.out.println("help");
+		imgBG = con.loadImage("images/help1.jpg");
+		con.drawImage(imgBG,0,0);
+		con.repaint();
+		boolean blnNextHelp = false;
+		while(chrInput != 'b'){
+			chrInput = con.getChar();
+			if(chrInput == 'n' && !blnNextHelp){
+				imgBG = con.loadImage("images/help2.jpg");
+				con.drawImage(imgBG,0,0);
+				con.repaint();
+				blnNextHelp = true;
+			}
+		}
+		imgBG = con.loadImage("images/menu.jpg");
+		con.drawImage(imgBG,0,0);
+		con.repaint();
 	}
 	public static void viewScores(){
-		
+		System.out.println("view scores");
+		imgBG = con.loadImage("images/highScoreBG.jpg");
+		con.drawImage(imgBG,0,0);
+		con.repaint();
+		TextInputFile txtScoresInput = new TextInputFile("scores.txt");
+		int intScoreCount = 1;
+		if(txtScoresInput.eof()){
+			con.println("There's no scores right now!");
+		}else{
+			while(!txtScoresInput.eof()){
+				con.println(intScoreCount+". "+txtScoresInput.readLine()+" - $"+txtScoresInput.readLine());
+				intScoreCount++;
+			}
+		}
+		//this is just to stall it so that it runs the rest when you press 'b'
+		while(chrInput != 'b'){		
+			chrInput = con.getChar();
+		}
+		imgBG = con.loadImage("images/menu.jpg");
+		con.drawImage(imgBG,0,0);
+		con.repaint();
+		con.clear();
 	}
-	//Different hand types
+	//secret joke
+	public static void secret(){
+		System.out.println("secret");
+		int intRandom = (int)(Math.random()*3+1);
+		BufferedImage imgDargon = con.loadImage("images/dargon"+intRandom+".png");
+		BufferedImage imgText = con.loadImage("images/text.gif");
+		if(intRandom == 1){
+			//dargon extrem sprots
+			for(int intCount = 1280; intCount >= -600; intCount -= 12){
+				con.drawImage(imgBG,0,0);
+				con.drawImage(imgText,intCount,500);
+				con.drawImage(imgDargon,intCount+86,150);
+				con.repaint();
+				con.sleep(33);
+			}
+		}else if(intRandom == 2){
+			//dargon flies up
+			for(int intCount = 720; intCount >= -300; intCount -= 20){
+				con.drawImage(imgBG,0,0);
+				con.drawImage(imgDargon,390,intCount);
+				con.repaint();
+				con.sleep(33);
+			}
+		}else{
+			//dargon big face goes in then out
+			for(int intCount = 1280; intCount >= 0; intCount -= 40){
+				con.drawImage(imgBG,0,0);
+				con.drawImage(imgDargon,intCount,0);
+				con.repaint();
+				con.sleep(33);
+			}
+			con.sleep(2000);
+			for(int intCount = 0; intCount <= 1280; intCount += 40){
+				con.drawImage(imgBG,0,0);
+				con.drawImage(imgDargon,intCount,0);
+				con.repaint();
+				con.sleep(33);
+			}
+		}
+	}
+	//Different hand type checks
 	public static boolean pair(){
 		for(int intCount = 0; intCount < 4; intCount++){
 			if(intHand[intCount][0] == intHand[intCount+1][0] && intHand[intCount][0] > 10){
